5dd56a8614f7db5a94f864512e103d1bafd3c0fa
Lizethch <lizethchm4@gmail.com>
2024-07-18 20:35:28 -0500

    Upload component card

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import Card from "./components/Card/Card";
+import styles from './App.module.css';
+import pokeapiLogo from './assets/pokeapi.png';
+import tictactoeLogo from './assets/tictactoe.png';
+import wordleLogo from './assets/wordle.png';
+import youtubeLogo from './assets/youtube.png';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className={styles.appContainer}>
+      <Card
+        image={tictactoeLogo}
+        title="ReactDev Tic-Tac-Toe"
+        buttons={["useState", "useEffect", "Custom Hooks", "localStorage", "CSS Modules", "otherFeature"]}
+      />
+      <Card
+        image={pokeapiLogo}
+        title="Poke Collection"
+        buttons={["useState", "useEffect", "Custom Hooks", "localStorage", "CSS Modules", "otherFeature"]}
+      />
+      <Card
+        image={wordleLogo}
+        title="React Wordle"
+        buttons={["useState", "useEffect", "Custom Hooks", "localStorage", "CSS Modules", "otherFeature"]}
+      />
+      <Card
+        image={youtubeLogo}
+        title="Video Feed"
+        buttons={["useState", "useEffect", "Custom Hooks", "localStorage", "CSS Modules", "otherFeature"]}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
