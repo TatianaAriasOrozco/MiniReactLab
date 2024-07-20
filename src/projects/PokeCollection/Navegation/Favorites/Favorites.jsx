390b5d3bebb422186ea79b0810d85c14c0334af1
Franks <czfranks@gmail.com>
2024-07-20 11:57:52 -0500

    Update: card pokemon favorite styles

--- src/projects/PokeCollection/Navegation/Favorites/Favorites.jsx
+++ src/projects/PokeCollection/Navegation/Favorites/Favorites.jsx
@@ -1,9 +1,23 @@
 import { useEffect, useState } from 'react';
 import { useFetch } from '../../../../hooks/useFetch';
 import { URL_BASE } from '../../api/config';
+import CardPokemon from '../CardPokemon/CardPokemon';
+import styles from './Favorites.module.css';
 
 const Favorites = ({ favorites }) => {
-  return <div>{JSON.stringify(favorites)}</div>;
+  return (
+    <div className={styles.content}>
+      {favorites.map((pokemon) => (
+        <CardPokemon
+          key={pokemon.id}
+          name={pokemon.name}
+          id={pokemon.id}
+          img={pokemon.avatarUrl}
+          types={pokemon.types}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Favorites;
